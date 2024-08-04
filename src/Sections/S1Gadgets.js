0704a9163e6c5ad56a62a7501297e698a8eeb991
kannankkking <nagappanramu44@gamil.com>
2024-08-04 12:26:30 +0530

    add

--- src/Sections/S1Gadgets.js
+++ src/Sections/S1Gadgets.js
@@ -1,9 +1,325 @@
 import React from 'react'
+import styles from "../css/S1Gadgets.module.css"
+import { LuShoppingCart } from "react-icons/lu";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { CiHeart } from "react-icons/ci";
+import { FaLaptop } from 'react-icons/fa6';
+
+const logo = new URL("../images/image.jpeg",import.meta.url);
+
+// sector1//
+const airpods = new URL("../images/s1_2_1airpods.jpeg",import.meta.url);
+const headphone = new URL("../images/s1_2_1headphone.jpeg",import.meta.url);
+const laptop = new URL("../images/s1_2_1laptop.jpeg",import.meta.url);
+const remote= new URL("../images/s1_2_1remote.jpeg",import.meta.url);
+const smart = new URL("../images/s1_2_1smart.jpeg",import.meta.url);
+const wired = new URL("../images/s1_2_1wired.jpeg",import.meta.url);
+
+
+// sector2//
+const aroma= new URL("../images/s1_2_2aroma.jpeg",import.meta.url);
+const boat = new URL("../images/s1_2_2boat.jpeg",import.meta.url);
+const number = new URL("../images/s1_2_2number.jpeg",import.meta.url);
+const speaker= new URL("../images/s1_2_2speaker.jpeg",import.meta.url);
+const zebronics = new URL("../images/s1_2_2zebronics.jpeg",import.meta.url);
+const boult = new URL("../images/s1_2_2boult.jpeg",import.meta.url);
+
+// sector3//
+const camera3= new URL("../images/s1_2_3camera.jpeg",import.meta.url);
+const gamepad = new URL("../images/s1_2_3gamepad.jpeg",import.meta.url);
+const keyboard = new URL("../images/s1_2_3keyboard.jpeg",import.meta.url);
+const mini= new URL("../images/s1_2_3mini.jpeg",import.meta.url);
+const mouse = new URL("../images/s1_2_3mouse.jpeg",import.meta.url);
+const tab3= new URL("../images/s1_2_3tab.jpeg",import.meta.url);
+
+// sector4//
+const charger= new URL("../images/s1_2_4charger.jpeg",import.meta.url);
+const camera4 = new URL("../images/s1_2_4camera.jpeg",import.meta.url);
+const laptop4 = new URL("../images/s1_2_4laptop.jpeg",import.meta.url);
+const otg= new URL("../images/s1_2_4otg.jpeg",import.meta.url);
+const smart4 = new URL("../images/s1_2_4smart.jpeg",import.meta.url);
+const tab4 = new URL("../images/s1_2_4tab.jpeg",import.meta.url);
+
+// sector5//
+const holder= new URL("../images/s1_2_5holder.jpeg",import.meta.url);
+const power = new URL("../images/s1_2_5power.jpeg",import.meta.url);
+// const laptop4 = new URL("../images/s1_2_4laptop.jpeg",import.meta.url);
+// const otg= new URL("../images/s1_2_4otg.jpeg",import.meta.url);
+// const smart4 = new URL("../images/s1_2_4smart.jpeg",import.meta.url);
+// const tab4 = new URL("../images/s1_2_4tab.jpeg",import.meta.url);
 
 const S1Gadgets = () => {
   return (
-    <div>
-      
+    <div id={styles.heading}>
+        <div className={styles.Header}>
+     <div className={styles.navbar}> 
+    <img src={logo}/>
+  <input type='text'placeholder='Search for products, Brands and More'/>
+
+
+<select >
+ <option className={styles.login}>Login</option> 
+  <option>New Customer ?</option>
+  <option>My Profile</option>
+  <option>Wishlist</option>
+</select>
+
+<button><LuShoppingCart id={styles.icon}/>Cart</button>
+
+<BsThreeDotsVertical className={styles.icon} />
+
+    </div>
+    </div>
+{/* sector1 */}
+
+<h1></h1>
+    <div className={styles.sector1}>
+
+  <div className={styles.set1}>
+     <img src={airpods}/><CiHeart className={styles.heart}/>
+      <h5> Boult Airpods</h5>
+     <p>4.5*</p>
+     <span>$30</span>
+  </div>
+
+  <div className={styles.set1}>
+  <img src={headphone}/><CiHeart className={styles.heart}/>
+  <h5>Wireless Headphones</h5>
+  <p>4.5*</p>
+  <span>$45</span>
+  </div>
+
+  <div className={styles.set1}>
+  <img src={laptop}/><CiHeart className={styles.heart}/>
+  <h5>Asus Laptop<br/>(256 GB)</h5>
+  <p>4.5*</p>
+  <span>$50.000</span>
+  </div>
+
+  <div className={styles.set1}>
+  <img src={remote}/><CiHeart className={styles.heart}/>
+  <h5>LED Tv Remote</h5>
+  <p>4.5*</p>
+  <span>$10</span>
+  </div>
+
+  <div className={styles.set1}>
+  <img src={smart}/><CiHeart className={styles.heart}/>
+  <h5>Smart Watch</h5>
+  <p>4.2*</p>
+  <span>$35</span>
+  </div>
+
+
+  <div className={styles.set1}>
+  <img src={wired}/> <CiHeart className={styles.heart}/>
+  <h5>Boat wired Headset</h5>
+  <p>4.3*</p>
+  <span>$25</span>
+  </div>
+
+</div>
+
+
+{/* sector2 */}
+
+<div className={styles.sector2}>
+
+<div className={styles.set2}>
+   <img src={aroma}/><CiHeart className={styles.heart}/>
+    <h5>Aroma Airpods</h5>
+   <p>4.2*</p>
+   <span>$30</span>
+</div>
+
+<div className={styles.set2}>
+<img src={boat}/><CiHeart className={styles.heart}/>
+<h5>Boat Headphone Wirless</h5>
+<p>4.1*</p>
+<span>$45</span>
+</div>
+
+<div className={styles.set2}>
+<img src={number}/><CiHeart className={styles.heart}/>
+<h5>Number Airpods</h5>
+<p>4.4*</p>
+<span>$50.000</span>
+</div>
+
+<div className={styles.set2}>
+<img src={speaker}/><CiHeart className={styles.heart}/>
+<h5>Wirless Speaker</h5>
+<p>4.5*</p>
+<span>$10</span>
+</div>
+
+<div className={styles.set2}>
+<img src={zebronics}/><CiHeart className={styles.heart}/>
+<h5>Zebronics</h5>
+<p>4.2*</p>
+<span>$35</span>
+</div>
+
+
+<div className={styles.set2}>
+<img src={boult}/> <CiHeart className={styles.heart}/>
+<h5>Boult</h5>
+<p>4.3*</p>
+<span>$25</span>
+</div>
+
+</div>
+
+
+
+{/* sector3 */}
+
+<div className={styles.sector3}>
+
+<div className={styles.set3}>
+   <img src={camera3}/><CiHeart className={styles.heart}/>
+    <h5>Canon Camera</h5>
+   <p>4.4*</p>
+   <span>$40</span>
+</div>
+
+<div className={styles.set3}>
+<img src={gamepad}/><CiHeart className={styles.heart}/>
+<h5>Gaming Pad</h5>
+<p>4.1*</p>
+<span>$35</span>
+</div>
+
+<div className={styles.set3}>
+<img src={keyboard}/><CiHeart className={styles.heart}/>
+<h5>Gaing Keyboard</h5>
+<p>4.5*</p>
+<span>$20</span>
+</div>
+
+<div className={styles.set3}>
+<img src={mini}/><CiHeart className={styles.heart}/>
+<h5>Mini Camera</h5>
+<p>4.5*</p>
+<span>$10</span>
+</div>
+
+<div className={styles.set3}>
+<img src={mouse}/><CiHeart className={styles.heart}/>
+<h5>Wirless Mouse</h5>
+<p>4.2*</p>
+<span>$35</span>
+</div>
+
+
+<div className={styles.set3}>
+<img src={tab3}/> <CiHeart className={styles.heart}/>
+<h5>Samsung Tab</h5>
+<p>4.3*</p>
+<span>$25</span>
+</div>
+
+</div>
+
+
+{/* sector4 */}
+
+<div className={styles.sector4}>
+
+<div className={styles.set4}>
+   <img src={charger}/><CiHeart className={styles.heart}/>
+    <h5>C Type Charger</h5>
+   <p>4.5*</p>
+   <span>$30</span>
+</div>
+
+<div className={styles.set4}>
+<img src={camera4}/><CiHeart className={styles.heart}/>
+<h5>Camera</h5>
+<p>4.5*</p>
+<span>$55</span>
+</div>
+
+<div className={styles.set4}>
+<img src={laptop4}/><CiHeart className={styles.heart}/>
+<h5>Lenova Laptop</h5>
+<p>4.5*</p>
+<span>$70.00</span>
+</div>
+
+<div className={styles.set4}>
+<img src={otg}/><CiHeart className={styles.heart}/>
+<h5>OTG</h5>
+<p>4.5*</p>
+<span>$10</span>
+</div>
+
+<div className={styles.set4}>
+<img src={smart4}/><CiHeart className={styles.heart}/>
+<h5>Noise Smart Watch</h5>
+<p>4.2*</p>
+<span>$35</span>
+</div>
+
+
+<div className={styles.set4}>
+<img src={tab4}/> <CiHeart className={styles.heart}/>
+<h5>Lenova Tab</h5>
+<p>4.3*</p>
+<span>$25</span>
+</div>
+
+</div>
+
+
+{/* sector5 */}
+
+<div className={styles.sector5}>
+
+<div className={styles.set5}>
+   <img src={holder}/><CiHeart className={styles.heart}/>
+    <h5>Phone Holder</h5>
+   <p>4.5*</p>
+   <span>$30</span>
+</div>
+
+<div className={styles.set5}>
+<img src={power}/><CiHeart className={styles.heart}/>
+<h5>Power Bank</h5>
+<p>4.5*</p>
+<span>$45</span>
+</div>
+
+<div className={styles.set5}>
+<img src={number}/><CiHeart className={styles.heart}/>
+<h5>Number Airpods</h5>
+<p>4.5*</p>
+<span>$50</span>
+</div>
+
+<div className={styles.set5}>
+<img src={speaker}/><CiHeart className={styles.heart}/>
+<h5>Wirless Speaker</h5>
+<p>4.5*</p>
+<span>$10</span>
+</div>
+
+<div className={styles.set5}>
+<img src={zebronics}/><CiHeart className={styles.heart}/>
+<h5>Zebronics</h5>
+<p>4.2*</p>
+<span>$35</span>
+</div>
+
+
+<div className={styles.set5}>
+<img src={boult}/> <CiHeart className={styles.heart}/>
+<h5>Boult</h5>
+<p>4.3*</p>
+<span>$25</span>
+</div>
+
+</div>
     </div>
   )
 }
--- src/Sections/S1Mobiles.js
+++ src/Sections/S1Mobiles.js
@@ -3,12 +3,27 @@ import styles from "../css/S1Mobiles.module.css"
 import { useNavigate } from 'react-router-dom';
 import { LuShoppingCart } from "react-icons/lu";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { CiHeart } from "react-icons/ci";
+import { FaFacebook } from "react-icons/fa6";
+import { RiTwitterXLine } from "react-icons/ri";
+import { FaYoutube } from "react-icons/fa";
+
 const logo = new URL("../images/image.jpeg",import.meta.url);
-const s1_1miphone = new URL("../images/s1.1miphone.jpeg",import.meta.url);
+const s1_1miphone = new URL("../images/s1.1m5g.jpeg",import.meta.url);
 const s1_1mnothing = new URL("../images/s1.1mnothing.jpg",import.meta.url);
 const s1_1moppo= new URL("../images/s1.1moppo.jpg",import.meta.url);
 const s1_1msale= new URL("../images/s1.1msale.jpg",import.meta.url);
-const s1_1msamsung = new URL("../images/s1.1msamsung.jpg",import.meta.url);
+const s1_1msamsung = new URL("../images/s1.1msamsung.jpg",import.meta.url);  
+
+// sector1//
+const motorola1 = new URL("../images/s1_1motorola1.jpeg",import.meta.url);
+const motorola2 = new URL("../images/s1_1motorola2.jpeg",import.meta.url);
+const motorola3 = new URL("../images/s1_1motorola3.jpeg",import.meta.url);
+const motorola4= new URL("../images/s1_1motorola4.jpeg",import.meta.url);
+const motorola5 = new URL("../images/s1_1motorola5.jpeg",import.meta.url);
+const motorola6 = new URL("../images/s1_1motorola6.jpeg",import.meta.url);
+
+
 
 // sector2//
 const fashion = new URL("../images/fashion.jpeg",import.meta.url);
@@ -20,6 +35,55 @@ const wheeler = new URL("../images/two wheeler.jpeg",import.meta.url);
 const gadgets = new URL("../images/gadgets.jpeg",import.meta.url);
 
 
+// sector3//
+const poco1 = new URL("../images/s1_1poco1.jpeg",import.meta.url);
+const poco2 = new URL("../images/s1_1poco2.jpeg",import.meta.url);
+const poco3 = new URL("../images/s1_1poco3.jpeg",import.meta.url);
+const poco4 = new URL("../images/s1_1poco4.jpeg",import.meta.url);
+const poco5 = new URL("../images/s1_1poco5.jpeg",import.meta.url);
+const poco6 = new URL("../images/s1_1poco6.jpeg",import.meta.url);
+
+
+// sector4//
+const vivo1 = new URL("../images/s1_1realme1.jpeg",import.meta.url);
+const vivo2 = new URL("../images/s1_1realme2.jpeg",import.meta.url);
+const vivo3 = new URL("../images/s1_1realme3.jpeg",import.meta.url);
+const vivo4 = new URL("../images/s1_1realme4.jpeg",import.meta.url);
+const vivo5 = new URL("../images/s1_1realme5.jpeg",import.meta.url);
+const vivo6 = new URL("../images/s1_1realme6.jpeg",import.meta.url);
+
+
+// sector5//
+const realme1 = new URL("../images/s1_1realme1.jpeg",import.meta.url);
+const realme2 = new URL("../images/s1_1realme2.jpeg",import.meta.url);
+const realme3 = new URL("../images/s1_1realme3.jpeg",import.meta.url);
+const realme4 = new URL("../images/s1_1realme4.jpeg",import.meta.url);
+const realme5 = new URL("../images/s1_1realme5.jpeg",import.meta.url);
+const realme6 = new URL("../images/s1_1realme6.jpeg",import.meta.url);
+
+// sector6//
+const iphone1 = new URL("../images/s1_1iphone1.jpeg",import.meta.url);
+const iphone2 = new URL("../images/s1_1iphone2.jpeg",import.meta.url);
+const iphone3 = new URL("../images/s1_1iphone3.jpeg",import.meta.url);
+const iphone4 = new URL("../images/s1_1iphone4.jpeg",import.meta.url);
+const iphone5 = new URL("../images/s1_1iphone5.jpeg",import.meta.url);
+const iphone6 = new URL("../images/s1_1iphone6.jpeg",import.meta.url);
+
+// sector7//
+const samsung1 = new URL("../images/s1_1samsung1.jpeg",import.meta.url);
+const samsung2 = new URL("../images/s1_1samsung2.jpeg",import.meta.url);
+const samsung3 = new URL("../images/s1_1samsung3.jpeg",import.meta.url);
+const samsung4 = new URL("../images/s1_1samsung4.jpeg",import.meta.url);
+const samsung5 = new URL("../images/s1_1samsung5.jpeg",import.meta.url);
+const samsung6 = new URL("../images/s1_1samsung6.jpeg",import.meta.url);
+
+// sector8//
+const redmi1 = new URL("../images/s1_1redmi1.jpeg",import.meta.url);
+const redmi2 = new URL("../images/s1_1redmi2.jpeg",import.meta.url);
+const redmi3 = new URL("../images/s1_1redmi3.jpeg",import.meta.url);
+const redmi4 = new URL("../images/s1_1redmi4.jpeg",import.meta.url);
+const redmi5 = new URL("../images/s1_1redmi5.jpeg",import.meta.url);
+const redmi6 = new URL("../images/s1_1redmi6.jpeg",import.meta.url);
 
 function S1Mobiles () {
   const navigate = useNavigate();
@@ -71,35 +135,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+     <img src={motorola1}/><CiHeart id={styles.heart}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={motorola2}/><CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={motorola3}/><CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={motorola4}/><CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={motorola5}/><CiHeart id={styles.heart}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -107,7 +171,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={motorola6}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -122,35 +186,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+     <img src={poco1}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={poco2}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={poco3}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={poco4}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={poco5}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -158,7 +222,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={poco6}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -172,35 +236,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+   <img src={vivo1}/> <CiHeart id={styles.heart}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={vivo2}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={vivo3}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={vivo4}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={vivo5}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -208,7 +272,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={vivo6}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -224,35 +288,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+     <img src={realme1}/> <CiHeart id={styles.heart}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={realme2}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={realme3}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={realme4}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={realme5}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -260,7 +324,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={realme6}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -275,35 +339,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+     <img src={iphone1}/> <CiHeart id={styles.heart}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={iphone2}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={iphone3}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={iphone4}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={iphone5}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -311,7 +375,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={iphone6}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -326,35 +390,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+     <img src={samsung1}/> <CiHeart id={styles.heart}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={samsung2}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={samsung3}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={samsung4}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={samsung5}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -362,7 +426,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={samsung6}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -377,35 +441,35 @@ const handleLogin =(event)=>{
 <div className={styles.sector2}>
 
   <div id={styles.set1}>
-     <img src={fashion}/>
+     <img src={redmi1}/> <CiHeart id={styles.heart}/>
       <h5>Motorola G85 5G<br/>(Cobalt blue,128 GB)</h5>
      <p>4.5*</p>
      <span>$25.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={home}/>
+  <img src={redmi2}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(olivia Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={appliances}/>
+  <img src={redmi3}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G85 5G<br/>(Urban Grey,128 GB)</h5>
   <p>4.5*</p>
   <span>$20.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={beauty}/>
+  <img src={redmi4}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Mint Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$45.000</span>
   </div>
 
   <div id={styles.set1}>
-  <img src={travel}/>
+  <img src={redmi5}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G34 5G<br/>(Ocean Green,128 GB)</h5>
   <p>4.5*</p>
   <span>$35.000</span>
@@ -413,7 +477,7 @@ const handleLogin =(event)=>{
 
 
   <div id={styles.set1}>
-  <img src={gadgets}/>
+  <img src={redmi6}/> <CiHeart id={styles.heart}/>
   <h5>Motorola G64 5G<br/>(Pearl blue,128 GB)</h5>
   <p>4.5*</p>
   <span>$25.000</span>
@@ -430,14 +494,37 @@ const handleLogin =(event)=>{
 <div id={styles.left}>
   <span>About</span>
    <ul>
-    <li>Contact us</li>
-    <li>About us</li>
-    <li>Careers</li>
-    <li>Press</li>
-    <li>Information</li>
+   <a href="contact"> <li>Contact us</li></a>
+   <a href="#"> <li>About us</li></a>
+   <a href="/careers"> <li>Careers</li></a>
+   <a href="press"> <li>Press</li></a>
+   <a href="info"><li>Information</li></a>
    </ul>
 </div>
 
+<hr id={styles.hr}/>
+
+
+<div id={styles.center}>
+<span>Help</span>
+   <ul>
+   <a href="contact"> <li>Payments</li></a>
+   <a href="#"> <li>Shipping</li></a>
+   <a href="/careers"> <li>cancellation&Returns</li></a>
+   <a href="press"> <li>FAQ</li></a>
+   <a href="info"><li>Report infringment</li></a>
+   </ul>
+
+   <div id={styles.social}>
+<h3>Social</h3>
+<FaYoutube />
+<FaFacebook />
+<RiTwitterXLine />
+</div>
+</div>
+
+
+<hr className={styles.hr}/>
 <div id={styles.right}>
      <p>Mail Us</p>
 
@@ -454,8 +541,8 @@ Bengaluru, 560103,
 Karnataka, India
 </span>
 
-<p>Registered Office Address:</p>
-<span>Flipkart Internet Private Limited,
+<p> Address:</p>
+<span>ShopZone Internet Private Limited,
 
 Buildings Alyssa, Begonia &
 
@@ -474,15 +561,8 @@ Telephone: 044-45614700 / 044-67415800
 </div>
 </div>
 
-
-
-
-
-
-
-
     </div>
   )
 }
 
-export default S1Mobiles
+export default S1Mobiles;
